37085cf41f7649bfe88a3c56d0b247a73afe2e9b
Golden Eagle <joowoon.kang@biola.edu>
2020-03-13 10:33:18 -0700

    finished cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,73 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardHere(obj){
+    const newHeader = document.createElement("div"),
+          newHeadline = document.createElement("div"),
+          newAuthorLine = document.createElement("div"),
+          imgContainerDiv = document.createElement("div"),
+          imgTag = document.createElement("img"),
+          authorSpan = document.createElement("span");
+
+    newHeader.classList.add("card");
+    newHeadline.classList.add("headline");
+    newAuthorLine.classList.add("author");
+    imgContainerDiv.classList.add("img-container");
+
+
+    newHeader.textContent = obj.headline;
+    imgTag.setAttribute("src",obj.authorPhoto);
+    authorSpan.textContent = obj.authorName;
+
+    newHeader.append(newHeadline, newAuthorLine);
+    newAuthorLine.append(imgContainerDiv, authorSpan);
+    imgContainerDiv.append(imgTag);
+    
+
+
+    return newHeader
+}
+
+// console.log(cardHere(js))
+parentTagThree = document.querySelector(".cards-container")
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+           let js = response.data.articles.javascript.forEach(each => {
+                console.log(each)
+                parentTagThree.append(cardHere(each)) 
+            }) 
+            let bs = response.data.articles.bootstrap.forEach(each => {
+                console.log(each)
+                parentTagThree.append(cardHere(each)) 
+            })
+            let tech = response.data.articles.technology.forEach(each => {
+                console.log(each)
+                parentTagThree.append(cardHere(each)) 
+            })
+            let jq = response.data.articles.jquery.forEach(each => {
+                console.log(each)
+                parentTagThree.append(cardHere(each)) 
+            })
+            let node = response.data.articles.node.forEach(each => {
+                console.log(each)
+                parentTagThree.append(cardHere(each)) 
+            })
+            console.log(response);
+            // console.log(js)
+            
+           
+        // parentTagThree.append(cardHere(response));
+            }
+           
+        // {response.data.topics.forEach(topicEach =>{
+            // console.log(topicEach)
+            // parentTagTwo.append(tabsComponent(topicEach))
+            // console.log(parentTagTwo)
+            // })}
+        ).catch(error => {
+            console.log("the data was not returned", error);
+        })
+
+        
